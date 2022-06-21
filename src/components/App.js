a9468a74cedf06266ab565b869fc0a53c45f52ec
k98a73 <100816862+k98a73@users.noreply.github.com>
2022-06-21 13:55:57 +0900

    Providerのセットアップ

--- src/components/App.js
+++ src/components/App.js
@@ -4,15 +4,20 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import reducer from "../reducers/index.js";
 import EventForm from "./EventForm.js";
 import Events from "./Events.js";
+import AppContext from "../contexts/AppContext.js";
+
+console.log({ AppContext });
 
 const App = () => {
   const [state, dispatch] = useReducer(reducer, []);
 
   return (
-    <div className="container-fluid">
-      <EventForm state={state} dispatch={dispatch} />
-      <Events state={state} dispatch={dispatch} />
-    </div>
+    <AppContext.Provider value={"Hello, I am a Provider."}>
+      <div className="container-fluid">
+        <EventForm state={state} dispatch={dispatch} />
+        <Events state={state} dispatch={dispatch} />
+      </div>
+    </AppContext.Provider>
   );
 };
 
